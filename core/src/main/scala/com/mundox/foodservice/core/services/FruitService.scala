ce6234dbb6fe08b9328e41b59a4b21229ce9e34f
Milenio X <jnkmiloq@gmail.com>
2023-07-11 13:32:13 -0500

    Updated Fruit service to handle errors

--- core/src/main/scala/com/mundox/foodservice/core/services/FruitService.scala
+++ core/src/main/scala/com/mundox/foodservice/core/services/FruitService.scala
@@ -4,7 +4,7 @@ import com.mundox.foodservice.core.domain.Fruit
 
 trait FruitService[F[_]] {
 
-  def getFruits(): F[List[Fruit]]
+  def getFruits(): F[Either[String, List[Fruit]]]
 
-  def getFruitById(id: Int): F[Option[Fruit]]
+  def getFruitById(id: Int): F[Either[String, Option[Fruit]]]
 }
